276c4a78f838bfc1bbb589eb13ae9de8f4799f2d
daniellask2400 <daniellask2400@gmail.com>
2024-08-26 04:52:19 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,31 @@
+function generateListing() {
+    const color = document.getElementById('color').value;
+    const style = document.getElementById('style').value;
+    const brand = document.getElementById('brand').value;
+    const condition = document.getElementById('condition').value;
+    const price = document.getElementById('price').value;
+    const dimensions = document.getElementById('dimensions').value;
+
+    const title = `[Free Delivery 🚚] Beautiful ${color} '${brand}' ${style}`;
+    
+    const description = `
+        This stylish ${brand} ${style} is in excellent condition.
+        
+        ${dimensions ? 'Dimensions: ' + dimensions + '\n' : ''}
+        
+        •9/10 comfort rating
+        •Beautiful Modern Design
+        •No damage/rips/smells | ${condition} home
+        •High quality will last years.
+        
+        Asking $${price} Cash
+        
+        FREE Home Delivery Included🏠
+        (pay after delivery obviously)
+        
+        Need gone ASAP
+    `;
+
+    document.getElementById('title').innerText = title;
+    document.getElementById('description').innerText = description;
+}
